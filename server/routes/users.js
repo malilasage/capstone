e6e48eb54871138350223a50cf4620538b5caf84
malilasage <malilaclearwater@gmail.com>
2017-02-24 14:40:22 -0700

    server routes to user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,13 @@
 "use strict";
 
-// const express = require('express');
-// const router = express.Router();
-// const mongoose = require('mongoose');
-// const User = require('../src/users');
-// mongoose.Promise = require('bluebird');
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../src/users');
+mongoose.Promise = require('bluebird');
+
+router.get('/', (req, res) => {
+  res.send('coolio');
+});
+
+module.exports = router;
