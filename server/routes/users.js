db33ed631932dc6772daacdecba191c46c966d1a
malilasage <malilaclearwater@gmail.com>
2017-02-24 14:53:40 -0700

    get and post user works

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,8 +6,45 @@ const mongoose = require('mongoose');
 const User = require('../src/users');
 mongoose.Promise = require('bluebird');
 
-router.get('/', (req, res) => {
-  res.send('coolio');
+//gets all of a users data
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    console.log(req.params.id);
+    if(err) { res.send(404) }
+    else {
+      res.send(data);
+    }
+  })
+});
+
+router.post('/', (req, res) => {
+  User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    hashedPassword: req.body.hashedPassword
+  }, (err, data) => {
+    if(err) {throw err};
+    res.send(data);
+  })
+});
+
+router.patch('/:id', (req, res) => {
+  User.findByIdAndUpdate(req.params.id,
+    { $set: req.body }, { new: true },
+    function (err, data) {
+      if (err) throw err;
+      res.send(data);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    if(err) throw err;
+    data.remove((err, data) => {
+      if(err) throw err;
+      res.send(data);
+    })
+  })
 });
 
 module.exports = router;
