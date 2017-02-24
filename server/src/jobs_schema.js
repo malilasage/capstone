0390e8eaac6e9dff77548b2fe04b282aa29b2a42
malilasage <malilaclearwater@gmail.com>
2017-02-24 14:16:30 -0700

    created job schema

--- server/src/jobs_schema.js
+++ server/src/jobs_schema.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const JobsSchema = new Schema({
-
+  title: String,
+  description: String,
+  company: String,
+  notes: String,
+  tasks: {
+    resumeStatus: String,
+    coverLetterStatus: String,
+    interview: {
+      status: String,
+      date: String
+    }
+    deadline: String
+  }
+  createdAt: { type: Date, default: Date.now}
 });
 
 module.exports = JobsSchema;
