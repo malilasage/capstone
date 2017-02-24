9922c5162d54b26a9c18b136c8ab618079efb707
malilasage <malilaclearwater@gmail.com>
2017-02-24 14:11:38 -0700

    created user schema

---
+++ server/src/jobs_schema.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const JobsSchema = new Schema({
+
+});
+
+module.exports = JobsSchema;
---
+++ server/src/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const JobsSchema = require('./jobs_schema');
+
+const UserSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  hashedPassword: {
+    type: String
+    validate: {
+      validator: (hashedPassword) => hashedPassword.length === 16,
+      message: 'if ur password isnt 16 characters, its probably not hashed'
+    },
+    required: [true, 'password is required']
+  },
+  createdAt: { type: Date, default: Date.now},
+  jobs: [JobsSchema]
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
