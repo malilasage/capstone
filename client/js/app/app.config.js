dfc995965b74b858d2aa7b8005acd173f765a8f8
malilasage <malilaclearwater@gmail.com>
2017-02-25 19:23:01 -0700

    added angular config

--- client/js/app/app.config.js
+++ client/js/app/app.config.js
@@ -1,5 +1,18 @@
 (function(){
   'use strict';
 
-  // angular.component.config(config)
+  angular.module.config(config);
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider){
+    $locationProvider.html5Mode(true)
+
+    $stateProvider
+      .state({
+        name: 'app',
+        abstract: true,
+        component: 'app',
+      })
+  }
 }())
